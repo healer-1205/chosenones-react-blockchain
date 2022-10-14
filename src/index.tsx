b157b7636dcd33b4b7b6718821db2b026ca744c2
healer-1205 <crazyhealer1205@gmail.com>
2022-10-14 11:48:36 +0800

    add sass and react-bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const { provider, webSocketProvider } = configureChains(defaultChains, [
+  publicProvider(),
+])
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <WagmiConfig client={wagmiClient}>
+        <App />
+      </WagmiConfig>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

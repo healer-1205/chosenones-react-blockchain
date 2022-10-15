43eea678f8ff67a659013ed4844ae733c8ae2b56
healer-1205 <crazyhealer1205@gmail.com>
2022-10-15 10:33:36 +0800

    finish team page

--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -1,8 +1,17 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Col, Container, Row } from "react-bootstrap"
+import { ThemeContext } from "../../context/ThemeContext"
+import { Images, svgIcons } from "../../assets"
 import "./Team.scss"
 
 export const Team: React.FC = () => {
+  const { dark } = useContext(ThemeContext)
+  const [isDarkImage, setIsDarkImage] = useState(true)
+  useEffect(() => {
+    if (dark === "theme-dark") {
+      setIsDarkImage(true)
+    } else setIsDarkImage(false)
+  }, [dark])
   return (
     <div className="team">
       <Container>
@@ -19,7 +28,225 @@ export const Team: React.FC = () => {
       </Container>
       <Container>
         <Row>
-          <Col sm={12} md={4} lg={4} className="team__Col"></Col>
+          <Col sm={12} md={4} lg={4} className="team__Col">
+            <div className="team__container">
+              <img src={Images.team1} alt="team1" className="team__container__image" />
+              {isDarkImage ? (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.darkTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.darkFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              ) : (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.lightTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.lightFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              )}
+            </div>
+          </Col>
+          <Col sm={12} md={4} lg={4} className="team__Col">
+            <div className="team__container">
+              <img src={Images.team2} alt="team1" className="team__container__image" />
+              {isDarkImage ? (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.darkTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.darkFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              ) : (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.lightTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.lightFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              )}
+            </div>
+          </Col>
+          <Col sm={12} md={4} lg={4} className="team__Col">
+            <div className="team__container">
+              <img src={Images.team3} alt="team1" className="team__container__image" />
+              {isDarkImage ? (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.darkTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.darkFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              ) : (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.lightTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.lightFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              )}
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12} md={4} lg={4} className="team__Col">
+            <div className="team__container">
+              <img src={Images.team4} alt="team1" className="team__container__image" />
+              {isDarkImage ? (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.darkTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.darkFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              ) : (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.lightTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.lightFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              )}
+            </div>
+          </Col>
+          <Col sm={12} md={4} lg={4} className="team__Col">
+            <div className="team__container">
+              <img src={Images.team5} alt="team1" className="team__container__image" />
+              {isDarkImage ? (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.darkTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.darkFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              ) : (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.lightTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.lightFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              )}
+            </div>
+          </Col>
+          <Col sm={12} md={4} lg={4} className="team__Col">
+            <div className="team__container">
+              <img src={Images.team6} alt="team1" className="team__container__image" />
+              {isDarkImage ? (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.darkTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.darkFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.darkBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              ) : (
+                <div className="team__container__socials">
+                  <a href="https://twitter.com/" target="_blank">
+                    <img src={svgIcons.lightTwitterIcon} alt="Twitter" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightMsgIcon} alt="Msg" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img src={svgIcons.lightFacebookIcon} alt="Facebook" className="team__container__socials__icon" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img src={svgIcons.lightBeIcon} alt="Be" className="team__container__socials__icon" />
+                  </a>
+                </div>
+              )}
+              <div></div>
+            </div>
+          </Col>
         </Row>
       </Container>
     </div>

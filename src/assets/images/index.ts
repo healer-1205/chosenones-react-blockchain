a485c254400b103d11ffc4d44a771f1f2c77b60e
healer-1205 <crazyhealer1205@gmail.com>
2022-10-16 21:48:54 +0800

    make roadmap page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -11,14 +11,14 @@ import team3 from "./teams/03.png"
 import team4 from "./teams/04.png"
 import team5 from "./teams/05.png"
 import team6 from "./teams/06.png"
-import loadmap01 from "./roadmap/01.png"
-import loadmap02 from "./roadmap/02.png"
-import loadmap03 from "./roadmap/03.png"
-import loadmap04 from "./roadmap/04.png"
-import loadmap05 from "./roadmap/05.png"
-import loadmap06 from "./roadmap/06.png"
-import loadmap07 from "./roadmap/07.png"
-import loadmap08 from "./roadmap/08.png"
+import roadmap01 from "./roadmap/01.png"
+import roadmap02 from "./roadmap/02.png"
+import roadmap03 from "./roadmap/03.png"
+import roadmap04 from "./roadmap/04.png"
+import roadmap05 from "./roadmap/05.png"
+import roadmap06 from "./roadmap/06.png"
+import roadmap07 from "./roadmap/07.png"
+import roadmap08 from "./roadmap/08.png"
 
 const Images = {
   aboutMainBgDark,
@@ -34,14 +34,14 @@ const Images = {
   team4,
   team5,
   team6,
-  loadmap01,
-  loadmap02,
-  loadmap03,
-  loadmap04,
-  loadmap05,
-  loadmap06,
-  loadmap07,
-  loadmap08,
+  roadmap01,
+  roadmap02,
+  roadmap03,
+  roadmap04,
+  roadmap05,
+  roadmap06,
+  roadmap07,
+  roadmap08,
 }
 
 export default Images
--- src/pages/Roadmap/Roadmap.tsx
+++ src/pages/Roadmap/Roadmap.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Col, Container, Row } from "react-bootstrap"
+import { Images } from "../../assets"
 import "./Roadmap.scss"
 
 export const Roadmap: React.FC = () => {
@@ -12,6 +13,48 @@ export const Roadmap: React.FC = () => {
           </Col>
         </Row>
       </Container>
+      <div className="roadmap__container">
+        <div className="roadmap__container__bg"></div>
+        <Container className="roadmap__container__image">
+          <Row>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap01} alt="roadmap1" className="roadmap-image" />
+            </Col>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap02} alt="roadmap2" className="roadmap-image" />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap03} alt="roadmap3" className="roadmap-image" />
+            </Col>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap04} alt="roadmap4" className="roadmap-image" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="roadmap__container">
+        <div className="roadmap__container__bg"></div>
+        <Container className="roadmap__container__image">
+          <Row>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap05} alt="roadmap5" className="roadmap-image" />
+            </Col>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap06} alt="roadmap6" className="roadmap-image" />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap07} alt="roadmap7" className="roadmap-image" />
+            </Col>
+            <Col sm={12} md={6} lg={6} className="image-container">
+              <img src={Images.roadmap08} alt="roadmap8" className="roadmap-image" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   )
 }
